f7d6b23bd24c7f52b442f12eb6bcdfc0dff4a3ec
ricardo <ricardo.dev1991@gmail.com>
2023-11-17 12:18:46 -0300

    ✨ feat(Realizada Questão 2.2.):

---
+++ pp-p001-paulo-ricardo/questão2.2.py
@@ -0,0 +1,12 @@
+import math
+
+factorial = math.factorial(30)
+    
+print(factorial) 
+
+#Saída da variável factorial = 265252859812191058636308480000000
+#valor máximo de um tipo inteiro em C/C++ = 2147483647
+
+
+
+
