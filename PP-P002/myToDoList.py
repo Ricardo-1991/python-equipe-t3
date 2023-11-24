1026b224a287e66cc43580981b06c930d80f2891
ricardo <ricardo.dev1991@gmail.com>
2023-11-23 21:30:36 -0300

    ✨ feat(Relizado os métodos de adicionar uma nova tarefa e mostrar uma nova tarefa.):
    Foi trocado de lista para dicionário o atributo da classe ToDoList

--- PP-P002/myToDoList.py
+++ PP-P002/myToDoList.py
@@ -1,6 +1,25 @@
 class ToDoList:
     def __init__(self):
-        self.tasks = []
+            self.tasks = {}
+            
+    def show_tasks(self):
+        if self.isEmpty_task():
+            print("Não há tarefas cadastradas.")
+        else:
+            print("\nTarefas:")
+            for task_id, (description, status) in self.tasks.items():
+                print(f"{task_id}.{description} [{status}]")
+            
+    def register_task(self, description):
+        if not description[0].isupper():
+            description = description.capitalize()
+
+        task_id = len(self.tasks) + 1
+        self.tasks[task_id] = (description, ' ')
+
+        print("Tarefa registrada!!!")
+
+
 
 todo_list = ToDoList()
 
