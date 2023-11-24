2cf16281a327654d7b82406465c4a8b4f09153ea
ricardo <ricardo.dev1991@gmail.com>
2023-11-23 21:11:53 -0300

    🎉 init(Iniciando o projeto. Criando a estrutura do menu inicial):

---
+++ PP-P002/myToDoList.py
@@ -0,0 +1,9 @@
+def print_menu():
+    print("\nMenu:")
+    print("1. Mostrar Tarefas")
+    print("2. Registrar Nova Tarefa")
+    print("3. Marcar Tarefa como Realizada")
+    print("4. Editar Tarefa")
+    print("0. Sair")
+    
+print_menu()
