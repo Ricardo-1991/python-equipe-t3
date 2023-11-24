f93f412aab668429ce728df7bc8aa0914f6b30c4
Ricardo <ricardo.dev1991@gmail.com>
2023-11-24 08:21:39 -0300

    ✨ feat(Adicionando lógica de acesso aos métodos):

--- PP-P002/myToDoList.py
+++ PP-P002/myToDoList.py
@@ -1,6 +1,9 @@
 class ToDoList:
     def __init__(self):
             self.tasks = {}
+
+    def isEmpty_task(self):
+        return not bool(self.tasks)
             
     def show_tasks(self):
         if self.isEmpty_task():
@@ -32,4 +35,33 @@ def print_menu():
     print("4. Editar Tarefa")
     print("0. Sair")
     
-print_menu()
+def main():
+    todo_list = ToDoList()
+
+    while True:
+        print_menu()
+        choice = input("Escolha uma opção: ")
+
+        if choice == '0':
+            break
+        elif choice == '1':
+            todo_list.show_tasks()
+        elif choice == '2':
+            new_task_description = input("Digite a descrição da nova tarefa: ")
+            todo_list.register_task(new_task_description)
+        elif choice == '3':
+            todo_list.mark_task_done()
+        elif choice == '4':
+            try:
+                task_id = int(input("Digite o identificador da tarefa a ser editada: "))
+            except ValueError:
+                print("Por favor, insira um número válido.")
+                continue
+            new_description = input("Digite a nova descrição da tarefa: ")
+            todo_list.edit_task(task_id, new_description)
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+if __name__ == "__main__":
+    main()
