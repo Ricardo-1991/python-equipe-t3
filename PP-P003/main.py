13cd87a3f07adb218d5dcbd00a8e303a3026d70a
ricardo <ricardo.dev1991@gmail.com>
2023-11-30 01:54:57 -0300

    ✨ feat(Realizando quetão 2, persistencia de dados. Adicionando classe Empregado.):

--- PP-P003/main.py
+++ PP-P003/main.py
@@ -1,15 +1,36 @@
 from supermarket import Supermercado
 from product import Produto
+from employee import Empregado
+
+def carregar_dados_empregados(filename):
+    with open(filename, 'r') as file:
+        linhas = file.readlines()
+
+    empregados = []
+    for linha in linhas:
+        dados = linha.strip().split(',')
+        nome, sobrenome, ano_nascimento, rg, ano_admissao, salario = dados
+        empregado = Empregado(nome, sobrenome, int(ano_nascimento), rg, int(ano_admissao), float(salario))
+        empregados.append(empregado)
+
+    return empregados
+
+def Reajusta_dez_porcento(empregados):
+    for empregado in empregados:
+        empregado.salario *= 1.1
 
 def menu():
     print("1. Inserir novo produto")
     print("2. Excluir produto cadastrado")
     print("3. Listar todos os produtos")
     print("4. Consultar preço de um produto")
-    print("5. Sair")
+    print("5. Listar todos os empregados")
+    print("6. Reajustar salários")
+    print("7. Sair")
 
 def main():
     supermercado = Supermercado()
+    empregados = carregar_dados_empregados('dados_empregados.txt')
 
     while True:
         menu()
@@ -41,6 +62,15 @@ def main():
                 print("Produto não encontrado.")
 
         elif opcao == "5":
+            print("Lista de Empregados:")
+            for empregado in empregados:
+                print(empregado)
+
+        elif opcao == "6":
+            Reajusta_dez_porcento(empregados)
+            print("Salários reajustados em 10%.")
+
+        elif opcao == "7":
             print("Saindo do programa. Até mais!")
             break
 
